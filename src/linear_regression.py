66f0e8a854876e6939caf9cbd8fc27511b4d9400
moodi <moodizone@gmail.com>
2024-12-21 15:07:54 +0330

    🚧 improve error handling

--- src/linear_regression.py
+++ src/linear_regression.py
@@ -10,27 +10,34 @@ def store_data_set():
     Dataset characteristics:
     https://www.kaggle.com/datasets/kumarajarshi/life-expectancy-who
     """
-    source_dir = kagglehub.dataset_download("kumarajarshi/life-expectancy-who")
-    destination_dir = get_absolute_path("data/raw")
-    print("✅ Download completed successfully")
+    try:
+        source_dir = kagglehub.dataset_download("kumarajarshi/life-expectancy-who")
+        destination_dir = get_absolute_path("data/raw")
+        print("✅ Download completed successfully")
 
-    # ensure the destination directory exists
-    os.makedirs(destination_dir, exist_ok=True)
+        # ensure the destination directory exists
+        os.makedirs(destination_dir, exist_ok=True)
 
-    # list all files in the source directory
-    files = os.listdir(source_dir)
+        # list all files in the source directory
+        files = os.listdir(source_dir)
 
-    for file in files:
-        source_file = os.path.join(source_dir, file)
-        destination_file = os.path.join(destination_dir, file)
+        for file in files:
+            source_file = os.path.join(source_dir, file)
+            destination_file = os.path.join(destination_dir, file)
 
-        if os.path.isfile(source_file):
-            shutil.copy(source_file, destination_file)
-            print(f"✅ '{file}' copied successfully")
+            if os.path.isfile(source_file):
+                shutil.copy(source_file, destination_file)
+                print(f"✅ '{file}' copied successfully")
+    except Exception as e:
+        print(f"❌ An error occurred:\n{e}")
 
 
 def read_data_set() -> pd.DataFrame:
     data_path = get_absolute_path("data/raw/Life Expectancy Data.csv")
+
+    if not os.path.exists(data_path):
+        print(f"❌ File not found: {data_path}")
+        return
     data = pd.read_csv(data_path)
     df = pd.DataFrame(data)
     return df
