bad75e290ce5040bd3dd06db53ee70144f1ed2e6
moodi <moodizone@gmail.com>
2025-01-01 11:39:59 +0330

    🚧 add regression model

--- src/utils/kaggle.py
+++ src/utils/kaggle.py
@@ -3,6 +3,7 @@ import kagglehub
 import shutil
 import os
 from src.utils.helpers import get_absolute_path
+from sklearn.preprocessing import OneHotEncoder
 
 
 def download_data_from_kaggle(url: str):
@@ -36,4 +37,20 @@ def read_data_set(path: str) -> pd.DataFrame:
         return
     data = pd.read_csv(data_path)
     df = pd.DataFrame(data)
+    return df
+
+
+def one_hot_encode(df: pd.DataFrame):
+    categorical_cols = df.select_dtypes(include=["object"]).columns.tolist()
+    encoder = OneHotEncoder(sparse_output=False, handle_unknown="ignore")
+    one_hot_encoded = encoder.fit_transform(df[categorical_cols])
+    one_hot_encoded = pd.DataFrame(
+        one_hot_encoded,
+        columns=encoder.get_feature_names_out(categorical_cols),
+        index=df.index,
+    )
+
+    # drop original columns and concatenate the encoded DataFrame
+    df.drop(categorical_cols, axis=1, inplace=True)
+    df = pd.concat([df, one_hot_encoded], axis=1)
     return df
